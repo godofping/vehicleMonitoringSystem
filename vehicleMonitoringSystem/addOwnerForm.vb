ad5e80bcbf819c5d79040a605930534e18b3b0ca
godofping <rexmicrosoft@yahoo.com.ph>
2018-10-24 10:35:40 +0800

    1

--- vehicleMonitoringSystem/addOwnerForm.vb
+++ vehicleMonitoringSystem/addOwnerForm.vb
@@ -1,4 +1,9 @@
-﻿Public Class addOwnerForm
+﻿Imports Emgu.CV
+
+
+Public Class addOwnerForm
+    Dim capture As New Capture
+
     Private Sub closeButton_Click(sender As Object, e As EventArgs) Handles closeButton.Click
         Me.Close()
     End Sub
